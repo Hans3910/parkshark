a7916b2819a8f9c903e0b6b9cc54dfaaaab1206a
gsaegom <guillermosaezgomez@gmail.com>
2020-11-18 09:29:59 +0100

    Set Swagger UI

--- src/main/java/com/jawsome/parkshark/ParksharkApplication.java
+++ src/main/java/com/jawsome/parkshark/ParksharkApplication.java
@@ -1,7 +1,11 @@
 package com.jawsome.parkshark;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ParksharkApplication {
@@ -10,4 +14,8 @@ public class ParksharkApplication {
         SpringApplication.run(ParksharkApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI configureOpenApi() {
+        return new OpenAPI().info(new Info().title("Parkshark by Jaw-some").description("The best project ever!"));
+    }
 }
